fe6f1d4eb12b6749dec40799a9fbd180423e5f51
AngelDOrtiz <aortizghs@gmail.com>
2021-08-08 20:40:12 -0700

    crud routes complete

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -33,4 +33,43 @@ describe('demo routes', () => {
     const res = await request(app).get(`/api/v1/users/${user.id}`);
     expect(res.body).toEqual(user);
   });
+
+  it('updates a user by id', async () => {
+
+    const blob = {
+      username: 'Blob',
+      password: '1234,',
+      scores: 0,
+    };
+    const user = await User.INSERT(blob);
+
+    const newBlob = {
+      scores: 1
+    };
+    
+    const res = await request(app).put(`/api/v1/users/${user.id}`).send(newBlob);
+    
+    expect(res.body).toEqual({
+      id: '1',
+      username: 'Blob',
+      password: '1234,',
+      scores: 1,
+    });
+  });
+
+  it('deletes a user by id', async () => {
+
+    const blob = {
+      username: 'Blob',
+      password: '1234,',
+      scores: 0,
+    };
+    const user = await User.INSERT(blob);
+    
+    const res = await request(app).delete(`/api/v1/users/${user.id}`);
+    
+    expect(res.body).toEqual({
+      message: 'This user was deleted'
+    });
+  });
 });
